3f4359faa6a6cee81dcd111434e51c28d71145a5
austinv11 <austinv11@gmail.com>
2017-05-18 22:02:47 -0400

    Forgot to send signals...

--- src/main/kotlin/com/austinv11/d4j/bot/extensions/OkHttpExtensions.kt
+++ src/main/kotlin/com/austinv11/d4j/bot/extensions/OkHttpExtensions.kt
@@ -23,6 +23,11 @@ inline fun <reified T> String.get(): Mono<T> = Mono.create {
 }
 
 fun String.download(to: File): Mono<Unit> = Mono.from {
-    val response = rest.newCall(Request.Builder().url(this).build()).execute()
-    IOUtils.copy(response!!.body()!!.byteStream()!!, to.outputStream())
+    try {
+        val response = rest.newCall(Request.Builder().url(this).build()).execute()
+        IOUtils.copy(response!!.body()!!.byteStream()!!, to.outputStream())
+        it.onNext(Unit)
+    } catch (e: Throwable) {
+        it.onError(e)
+    }
 }
