c9bd222a83c1eb81de8226f940a0b2acb0c6c8a1
austinv11 <austinv11@gmail.com>
2017-05-18 20:49:02 -0400

    Convert to okhttp

--- src/main/kotlin/com/austinv11/d4j/bot/command/impl/UpdateCommand.kt
+++ src/main/kotlin/com/austinv11/d4j/bot/command/impl/UpdateCommand.kt
@@ -8,9 +8,10 @@ import com.austinv11.d4j.bot.command.context
 import com.austinv11.d4j.bot.extensions.buffer
 import com.austinv11.d4j.bot.extensions.embed
 import com.austinv11.d4j.bot.restart
-import com.github.kittinunf.fuel.httpDownload
-import sx.blah.discord.util.RateLimitException
+import okhttp3.*
+import org.apache.commons.io.IOUtils
 import java.io.File
+import java.io.IOException
 
 const val DOWNLOAD_URL = "https://jitpack.io/com/github/austinv11/D4JBot/-SNAPSHOT/D4JBot--SNAPSHOT-all.jar"
 
@@ -35,28 +36,51 @@ class UpdateCommand() : CommandExecutor() {
         val currJar = File(JAR_PATH)
         val temp = File.createTempFile("bot", ".jar")
         currJar.renameTo(temp)
-        
-        DOWNLOAD_URL.httpDownload().destination { _, _ -> currJar }
-                .responseString { request, response, result -> 
-                    result.fold({ d -> 
-                        LOGGER.info("Updated! Restarting...")
-                        buffer { message.edit(context.embed.withDesc("Updated!").build()) }
-                        temp.delete()
-                        restart()
-                    }, { err ->
-                        LOGGER.warn("Unable to update!")
-                        temp.renameTo(currJar)
-                        err.printStackTrace()
-                        throw err
-                    })
-                    temp.delete()
-                    channel.typingStatus = false
-                }.timeout(5 * 60 * 1000)
-                .timeoutRead(5 * 60 * 1000)
-                .progress { readBytes, totalBytes ->
-                    val percentage = "%.2f".format((readBytes.toDouble()/totalBytes.toDouble()) * 100.toDouble()) + "%"
-                    try { message.edit(context.embed.withDesc("$percentage done").build()) } catch (e: RateLimitException) {}
-                    LOGGER.info("$percentage done")
-                }
+
+        OkHttpClient().newCall(Request.Builder().url(DOWNLOAD_URL).get().build()).enqueue(object: Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                LOGGER.warn("Unable to update!")
+                temp.renameTo(currJar)
+                e!!.printStackTrace()
+                cleanup()
+                throw e
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                IOUtils.copy(response!!.body()!!.byteStream()!!, currJar.outputStream())
+                LOGGER.info("Updated! Restarting...")
+                buffer { message.edit(context.embed.withDesc("Updated!").build()) }
+                temp.delete()
+                cleanup()
+                restart()
+            }
+
+            fun cleanup() {
+                channel.typingStatus = false
+            }
+        })
+
+//        DOWNLOAD_URL.httpDownload().destination { _, _ -> currJar }
+//                .responseString { request, response, result ->
+//                    result.fold({ d ->
+//                        LOGGER.info("Updated! Restarting...")
+//                        buffer { message.edit(context.embed.withDesc("Updated!").build()) }
+//                        temp.delete()
+//                        restart()
+//                    }, { err ->
+//                        LOGGER.warn("Unable to update!")
+//                        temp.renameTo(currJar)
+//                        err.printStackTrace()
+//                        throw err
+//                    })
+//                    temp.delete()
+//                    channel.typingStatus = false
+//                }.timeout(5 * 60 * 1000)
+//                .timeoutRead(5 * 60 * 1000)
+//                .progress { readBytes, totalBytes ->
+//                    val percentage = "%.2f".format((readBytes.toDouble()/totalBytes.toDouble()) * 100.toDouble()) + "%"
+//                    try { message.edit(context.embed.withDesc("$percentage done").build()) } catch (e: RateLimitException) {}
+//                    LOGGER.info("$percentage done")
+//                }
     }
 }
